db3e3743379c8bc9a256fd03ea4debd6997c55ec
Merijn <99054535@mydavinci.nl>
2020-03-17 13:16:04 +0100

    het tweede begin

--- character.php
+++ character.php
@@ -1,2 +1,6 @@
 <?php
+$conn = new PDO('mysql:host=localhost;dbname=login-test', $user, $pass);
+$statement = $conn->prepare('SELECT * FROM users WHERE username = :username AND password = :password');
+$statement->execute([':username' => $_POST['userName'], ':password' => $userpass]);
+$result = $statement->fetchAll();
 ?>
