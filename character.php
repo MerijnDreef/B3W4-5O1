1bef3a7f42e40215b9bf335b1e97cfcbd07d0583
Merijn <99054535@mydavinci.nl>
2020-03-17 14:59:58 +0100

    heb een klein begin gemaakt

--- character.php
+++ character.php
@@ -1,6 +1,17 @@
 <?php
-$conn = new PDO('mysql:host=localhost;dbname=login-test', $user, $pass);
-$statement = $conn->prepare('SELECT * FROM users WHERE username = :username AND password = :password');
-$statement->execute([':username' => $_POST['userName'], ':password' => $userpass]);
+$conn = new PDO('mysql:host=localhost;dbname=characters', $user, $pass);
+$statement = $conn->prepare('SELECT `name`, `avatar`, `color`, `health`, `attack`, `defense` FROM characters ORDER BY `name`');
+$statement->execute();
 $result = $statement->fetchAll();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img href="index.php.png">
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,3 +4,14 @@ $statement = $conn->prepare('SELECT * FROM users WHERE username = :username AND 
 $statement->execute([':username' => $_POST['userName'], ':password' => $userpass]);
 $result = $statement->fetchAll();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
