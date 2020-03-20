0ada5f7a7c23a25d9a8bc90350c3052b77e35c71
Merijn <99054535@mydavinci.nl>
2020-03-20 11:10:23 +0100

    nieuwe dingen toegevoegd bijna klaar met de main pagina

--- index.php
+++ index.php
@@ -22,13 +22,20 @@ catch(PDOException $e)
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="/B3W45O1/style.css">
+    <title>character knowledge</title>
 </head>
 <body>
+    <h1>Alle 10 characters uit de database</h1>
     <?php 
     foreach($result as $character){
-   echo "<img src='images/" . $character['avatar'] . "'>";
+   echo "<div class='doos'><img src='images/" . $character['avatar'] . "'>";
+   echo "<p id='naamText'>" . $character['name'] . "</p>";
+   echo "<p id='health'> health : " . $character['health'] . "</p>";
+   echo "<p id='attack'> attack : " . $character['attack'] . "</p>";
+   echo "<p id='defense'> defense : " . $character['defense'] . "</p></div>";
     }
+    include("footer.php");
    ?> 
 </body>
 </html>
