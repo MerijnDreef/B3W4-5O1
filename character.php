c95a4d9bb7c5454a4f6d425f4af7bbc5d0217a4d
Merijn <99054535@mydavinci.nl>
2020-03-20 13:43:26 +0100

    bijna klaar ben bezig met character

--- character.php
+++ character.php
@@ -1,17 +1,19 @@
 <?php
-$conn = new PDO('mysql:host=localhost;dbname=characters', $user, $pass);
-$statement = $conn->prepare('SELECT `name`, `avatar`, `color`, `health`, `attack`, `defense` FROM characters ORDER BY `name`');
-$statement->execute();
-$result = $statement->fetchAll();
+    $conn = new PDO("mysql:host=$dbservername;dbname=characters", $username, $password);
+    $stat = $conn->prepare("SELECT id, name, avatar, health, attack, defense FROM characters;");
+    $stat->execute();
+    $result = $stat->fetchAll();
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/B3W45O1/style.css">
+    <script src="https://kit.fontawesome.com/ca88a28f8c.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
-    <img href="index.php.png">
+    
 </body>
 </html>
--- index.php
+++ index.php
@@ -13,7 +13,7 @@ catch(PDOException $e)
     {
         echo "Connection failed: " . $e->getMessage();
     }
-        $stat = $conn->prepare("SELECT id, name, avatar, health, attack, defense FROM characters;");
+        $stat = $conn->prepare("SELECT id, name, avatar, health, attack, defense FROM characters order by name;");
         $stat->execute();
         $result = $stat->fetchAll();
 ?>
@@ -23,20 +23,21 @@ catch(PDOException $e)
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/B3W45O1/style.css">
+    <script src="https://kit.fontawesome.com/ca88a28f8c.js" crossorigin="anonymous"></script>
     <title>character knowledge</title>
 </head>
 <body>
     <h1>Alle 10 characters uit de database</h1>
     <?php 
     foreach($result as $character){
-   echo "<div class='doos'>" 
+   echo "<div class='doos'>";
    echo '<a href="character.php/?name=' . urlencode($character['name']) . '">';
    echo "<img src='images/" . $character['avatar'] . "'>";
    echo "<p id='naamText'>" . $character['name'] . "</p>";
-   echo "<p id='health'> health : " . $character['health'] . "</p>";
-   echo "<p id='attack'> attack : " . $character['attack'] . "</p>";
-   echo "<p id='defense'> defense : " . $character['defense'] . "</p>";
-   echo "<hr></div>";
+   echo "<p id='health'><i id='health' class='fas fa-heart'></i> health : " . $character['health'] . "</p>";
+   echo "<p id='attack'><i id='attack' class='fas fa-fist-raised'></i> attack : " . $character['attack'] . "</p>";
+   echo "<p id='defense'><i id='defense' class='fas fa-shield-alt'></i> defense : " . $character['defense'] . "</p>";
+   echo "<hr></a></div>";
     }
     include("footer.php");
    ?> 
